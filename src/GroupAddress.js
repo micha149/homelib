76dd491f2ad0d79de579f71ff104dc873322550b
Michael van Engelshoven <michael@van-engelshoven.de>
2013-01-30 17:44:23 +0100

    Fixed group addresses string parsing
     #Please enter the commit message for your changes. Lines starting

--- src/GroupAddress.js
+++ src/GroupAddress.js
@@ -1,5 +1,10 @@
-
-
+/**
+ * Object representation for a group address. An instance can be created with
+ * a serializes group address. This address will be stored as 2 bytes and can
+ * be used to build a bus telegram.
+ * Each group address needs a datatype instance for validating data before
+ * sending. An optional title can be stored for organization purposes.
+ */
 var GroupAddress = function(address, type, title) {
     if (!address) {
         throw new Error('Missing address parameter');
@@ -19,28 +24,28 @@ var GroupAddress = function(address, type, title) {
  *            +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
  *    16 bits | 7| 6| 5| 4| 3| 2| 1| 0| 7| 6| 5| 4| 3| 2| 1| 0|
  *            +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
- *            |  |   Main-   |           Sub Group            |
- *            +--+-----------+--------+-----------------------+
  *            |  |   Main-   | Middle-|       Sub Group       |
+ *            +--+-----------+--------+-----------------------+
+ *            |  |   Main-   |           Sub Group            |
  *            +--+-----------+--------+-----------------------+
  */
 GroupAddress.prototype.parseAddress = function (str) {
-    var a, b, c,
-        m = str.match(/^(\d*)\/(\d*)(?:\/(\d*))?$/),
-        result = -1;
+    var m = str.match(/^(\d*)\/(\d*)(?:\/(\d*))?$/),
+        result = [];
 
     if (m && typeof m[3] !== "undefined" && m[1] < 16 && m[2] < 8 && m[3] < 256) {    
-        a = (m[1] & 0x01f) << 11;
-        b = (m[2] & 0x07) << 8;
-        c = m[3] & 0xff;
-        result = a | b | c;
+
+        result[0] = ((m[1] & 15) << 3) | (m[2] & 7);
+        result[1] = m[3] & 0xff;
+        
     } else if (m && m[1] < 16 && m[2] < 2048) {
-        a = (m[1] & 0x7f) << 8;
-        b = m[2] & 0xff;
-        result = a | b;
+
+        result[0] = ((m[1] & 15) << 3) | ((m[2] & 1792) >> 8);
+        result[1] = m[2] & 255;
+        
     }
 
-    if (result > -1) {
+    if (result.length) {
         return result;
     }
     
--- test/GroupAddressTest.js
+++ test/GroupAddressTest.js
@@ -26,10 +26,13 @@ describe('GroupAddress', function () {
 
         describe('on main/sublevel addresses', function () {
              
-            it('turns a string into two octets', function() {
+            it('turns a string into two bytes', function() {
                 var ga = new GroupAddress('1/2/3', "1.001");
                 
-                assert.deepEqual(ga.parseAddress("4/13"), 0x040D);
+                assert.deepEqual(ga.parseAddress("4/13"), [0x20, 0x0D]);
+                assert.deepEqual(ga.parseAddress("15/1365"), [0x7d, 0x55]);
+                assert.deepEqual(ga.parseAddress("7/0"), [0x38, 0x00]);
+                assert.deepEqual(ga.parseAddress("0/0"), [0x00, 0x00]);
             });
             
             it('throws error if sublevel is to high', function() {
@@ -53,7 +56,10 @@ describe('GroupAddress', function () {
             it('turns a string into two octets', function() {
                 var ga = new GroupAddress('1/2/3', "1.001");
                 
-                assert.deepEqual(ga.parseAddress("4/2/155"), 0x229b);
+                assert.deepEqual(ga.parseAddress("4/2/155"),  [0x22, 0x9b]);
+                assert.deepEqual(ga.parseAddress("11/6/241"), [0x5e, 0xf1]);
+                assert.deepEqual(ga.parseAddress("1/0/0"),    [0x08, 0x00]);
+                assert.deepEqual(ga.parseAddress("0/0/0"),    [0x00, 0x00]);
             });
             
             it('throws error if sublevel is to high', function() {
