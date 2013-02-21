30ccf41b8586b40a3eed2f2559e593ba6d853a12
Michael van Engelshoven <michael@van-engelshoven.de>
2013-02-21 23:30:34 +0100

    Implemented physical address

--- src/PhysicalAddress.js
+++ src/PhysicalAddress.js
@@ -1,3 +1,5 @@
+var isArray = require('underscore').isArray;
+
 /**
  * Object representation for a group address. An instance can be created with
  * a serializes group address. This address will be stored as 2 bytes and can
@@ -6,15 +8,44 @@
  * sending. An optional title can be stored for organization purposes.
  */
 var PhysicalAddress = function(address) {
-    if (!address) {
-        throw new Error('Missing address parameter');
+    if (isArray(address) && address.length === 2 && address[0] < 256 && address[1] < 256) {
+        this._address = address;
+        return;
     }
-    
-    this.address = address;
+    this._address = this.parseAddress(address)
 }
 
 PhysicalAddress.prototype.getRaw = function() {
-    return [0x9b, 0x00];
+    return this._address;
+}
+
+/**
+ * Parses a physical address string into two bytes
+ *
+ *     +------------------------------------+------------------------------------+
+ *     |         OCTET 0 (high byte)        |         OCTET 1 (low byte)         |
+ *     +-----+----+----+----+---+---+---+---+-----+----+----+----+---+---+---+---+
+ *     | 128 | 64 | 32 | 16 | 8 | 4 | 2 | 1 | 128 | 64 | 32 | 16 | 8 | 4 | 2 | 1 |
+ *     +-----+----+----+----+---+---+---+---+-----+----+----+----+---+---+---+---+
+ *     |         Area       |      Line     |               Member               |
+ *     +--------------------+---------------+------------------------------------+
+ *
+ * @param  {String} str
+ * @return {Number[]} Array with two bytes
+ */
+PhysicalAddress.prototype.parseAddress = function (str) {
+    var m = str.match(/^(\d*)\.(\d*)\.(\d*)$/),
+        result = [];
+
+    if (m && m[1] < 16 && m[2] < 16 && m[3] < 256) {
+
+        result[0] = ((m[1] & 15) << 4) | (m[2] & 15);
+        result[1] = m[3] & 255;
+        
+        return result;
+    }
+    
+    throw new Error(str + " is not a valid physical address.");
 }
 
 module.exports = PhysicalAddress;
---
+++ test/PhysicalAddressTest.js
@@ -0,0 +1,68 @@
+var assert  = require("assert"),
+    sinon   = require("sinon"),
+    PhysicalAddress = require('../homelib').PhysicalAddress;
+
+describe('PhysicalAddress', function () {
+
+    it("is a constructor", function() {
+        assert.equal(typeof PhysicalAddress, "function");
+    })
+    
+    describe('constructor', function () {
+        
+        it('parses and stores addres', function() {
+            var adr = new PhysicalAddress('1.2.3');
+            assert.deepEqual(adr._address, [0x12, 0x03]);
+        });
+
+        it('accepts array with two bytes', function() {        
+            var adr = new PhysicalAddress([0x12, 0x03]);
+            assert.deepEqual(adr._address, [0x12, 0x03]);
+        });
+        
+        it('expect address', function() {
+            var adr;
+            assert.throws(function() {
+                var adr = new PhysicalAddress('17.2.3');
+            });
+            assert.throws(function() {
+                var adr = new PhysicalAddress([1,2,3]);
+            });
+        });
+        
+    });
+    
+    describe('#parse()', function () {
+             
+        it('turns a string into two octets', function() {
+            var adr = new PhysicalAddress('1.2.3');
+            
+            assert.deepEqual(adr.parseAddress("4.2.155"),  [0x42, 0x9b]);
+            assert.deepEqual(adr.parseAddress("11.6.241"), [0xb6, 0xf1]);
+            assert.deepEqual(adr.parseAddress("1.0.0"),    [0x10, 0x00]);
+            assert.deepEqual(adr.parseAddress("0.0.0"),    [0x00, 0x00]);
+        });
+        
+        it('throws error if area value is to high', function() {
+            var ga = new PhysicalAddress('1.2.3');
+            assert.throws(function() {
+                ga.parseAddress("16/2/2");
+            });
+        });
+        
+        it('throws error if area value is to high', function() {
+            var ga = new PhysicalAddress('1.2.3');
+            assert.throws(function() {
+                ga.parseAddress("4/16/2");
+            });
+        });
+        
+        it('throws error if member value is to high', function() {
+            var ga = new PhysicalAddress('1.2.3');
+            assert.throws(function() {
+                ga.parseAddress("4.2.260");
+            });
+        });
+    });
+    
+});
