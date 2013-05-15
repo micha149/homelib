c25c1b520533ab670e313dcd4936d42ea7b262b6
Michael van Engelshoven <michael@van-engelshoven.de>
2013-05-15 17:21:55 +0200

    Fixed error object
    Error name and stack trace didn't work as expected with node js. Fixed
    that.

--- src/Error/UnexpectedValueError.js
+++ src/Error/UnexpectedValueError.js
@@ -1,9 +1,12 @@
 var util = require('util');
 
-UnexpectedValueError = function() {
-    Error.apply(this, arguments)
+UnexpectedValueError = function(msg, constr) {
+    Error.captureStackTrace(this, constr || this)
+    this.message = msg || 'Error'
 }
 
 util.inherits(UnexpectedValueError, Error);
+
+UnexpectedValueError.prototype.name= "Unexpected Value Error";
 
 module.exports = UnexpectedValueError;
