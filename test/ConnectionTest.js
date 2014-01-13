bc6b2dec09f5052dd68d9b7a4cebf720f8c96447
Michael van Engelshoven <michael@van-engelshoven.de>
2014-01-13 08:51:44 +0100

    Refactored connection tests

--- test/ConnectionTest.js
+++ test/ConnectionTest.js
@@ -1,5 +1,5 @@
 var sinon = require('sinon'),
-    assert = require('assert'),
+    expect  = require('chai').expect,
     homelib = require('../homelib'),
     Connection = homelib.Connection,
     GroupAddress = homelib.GroupAddress,
@@ -9,43 +9,44 @@ var sinon = require('sinon'),
 
 describe('Connection', function() {
 
-    describe('constructor', function() {
+    var sandbox;
+
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+    describe('creating an instance', function() {
 
         it('needs a driver', function() {
             
             var driver     = sinon.createStubInstance(Driver),
                 connection = new Connection(driver);
 
-            assert.throws(function() {
+            expect(function() {
                 connection = new Connection();
-            });
+            }).to.Throw(Error);
         });
 
         it('binds callback to drivers `message` event', function() {
 
-            var driver     = sinon.createStubInstance(Driver),
-                driverMock = sinon.mock(driver, 'on'),
+            var driver = sinon.createStubInstance(Driver),
                 connection;
 
-            sinon.stub(Connection.prototype, '_onDriverMessage');
-
-            driver.on.restore();
-            driverMock.expects('on')
-                      .withArgs('message')
-                      .callsArg(1);
+            sandbox.stub(Connection.prototype, '_onDriverMessage');
+            driver.on.withArgs('message').yields();
 
             connection = new Connection(driver);
 
-            driverMock.verify();
-
-            assert.ok(connection._onDriverMessage.calledOnce, "Driver message callback bound");
-
-            Connection.prototype._onDriverMessage.restore();
+            expect(connection._onDriverMessage).to.be.calledOnce;
         });
 
     });
     
-    describe('.send()', function() {
+    describe('sending messages', function() {
         it('calls connect on driver if driver is not connected', function() {
 
             var driver     = sinon.createStubInstance(Driver),
@@ -56,8 +57,8 @@ describe('Connection', function() {
 
             connection.send(msg);
 
-            assert.ok(driver.isConnected.calledOnce, "Check for connection");
-            assert.ok(driver.connect.calledOnce, "Called connect");
+            expect(driver.isConnected).to.be.calledOnce;
+            expect(driver.connect).to.be.calledOnce.and.calledAfter(driver.isConnected);
         });
 
         it('calls not connect on driver if driver is already connected', function() {
@@ -70,7 +71,7 @@ describe('Connection', function() {
 
             connection.send(msg);
 
-            assert.ok(!driver.connect.called, "Connect wasn't called");
+            expect(driver.connect).not.to.be.called;
         });
 
         it('calls send() on driver', function() {
@@ -84,9 +85,8 @@ describe('Connection', function() {
 
             connection.send(msg, callback);
 
-            assert.ok(driver.send.calledOnce, "Send was called");
-            assert.deepEqual(driver.send.firstCall.args[0], msg, "Message passed");
-            assert.deepEqual(driver.send.firstCall.args[1], callback, "Callback passed");
+            expect(driver.send).to.be.calledOnce;
+            expect(driver.send).to.be.calledWith(msg, callback);
         });
 
         it('calls send() on driver after connection was established', function() {
@@ -97,19 +97,18 @@ describe('Connection', function() {
                 connection = new Connection(driver);
 
             driver.isConnected.returns(false);
+            driver.connect.yieldsOn(driver);
 
             connection.send(msg, callback);
 
-            assert.ok(!driver.send.called, "Send was not called yet");
-            driver.connect.firstCall.args[0].call(driver);
-            assert.ok(driver.send.calledOnce, "Send was called after connect");
-            assert.deepEqual(driver.send.firstCall.args[0], msg, "Message passed");
-            assert.deepEqual(driver.send.firstCall.args[1], callback, "Callback passed");
+            expect(driver.send).to.be.calledOnce
+                .and.calledAfter(driver.connect)
+                .and.calledWith(msg, callback);
         });
 
     });
     
-    describe('.on()', function() {
+    describe('listening to messages', function() {
 
         it('calls connect on driver if driver is not connected', function() {
 
@@ -121,8 +120,9 @@ describe('Connection', function() {
 
             connection.on(address, function() {});
 
-            assert.ok(driver.isConnected.calledOnce, "Check for connection");
-            assert.ok(driver.connect.calledOnce, "Called connect");
+            expect(driver.isConnected).to.be.calledOnce;
+            expect(driver.connect).to.be.calledOnce
+                .and.calledAfter(driver.isConnected);
         });
 
         it('calls not connect on driver if driver is already connected', function() {
@@ -135,69 +135,57 @@ describe('Connection', function() {
 
             connection.on(address, function() {});
 
-            assert.ok(!driver.connect.called, "Connect wasn't called");
+            expect(driver.connect).not.to.be.called;
         });
 
-        it('adds group address to the listeners array',  function() {
+        it('calls listener if driver emits message',  function() {
 
             var driver     = sinon.createStubInstance(Driver),
                 address    = sinon.createStubInstance(GroupAddress),
+                msg        = sinon.createStubInstance(Message),
                 rawAddress = [0x22, 0x9b],
-                callback   = function() {},
-                connection = new Connection(driver);
+                callback   = sinon.spy(),
+                connection;
+
+            driver.on.restore();
+            driver.emit.restore();
+
+            connection = new Connection(driver);
 
             address.getRaw.returns(rawAddress);
+            msg.getDestination.returns(address);
 
             connection.on(address, callback);
 
-            assert.equal(connection._listeners[rawAddress][0], callback);
-
+            driver.emit("message", msg);
+            expect(callback).to.be.calledOnce
+                .and.calledWith(msg);
         });
 
         it('is able to handle multiple callbacks',  function() {
 
             var driver     = sinon.createStubInstance(Driver),
                 address    = sinon.createStubInstance(GroupAddress),
-                rawAddress = [0x22, 0x9b],
-                callbackA   = function() {},
-                callbackB   = function() {},
-                connection = new Connection(driver);
-
-            address.getRaw.returns(rawAddress);
-
-            connection.on(address, callbackA);
-            connection.on(address, callbackB);
-
-            assert.equal(connection._listeners[rawAddress].length, 2, "Has two callbacks");
-            assert.equal(connection._listeners[rawAddress][0], callbackA, "Callback A added");
-            assert.equal(connection._listeners[rawAddress][1], callbackB, "Callback B added");
-        });
-    });
-
-    describe('_onDriverMessage()', function() {
-
-        it('triggers each callback for matching address', function() {
-
-            var driver     = sinon.createStubInstance(Driver),
-                address    = sinon.createStubInstance(GroupAddress),
-                message    = sinon.createStubInstance(Message),
+                msg        = sinon.createStubInstance(Message),
                 rawAddress = [0x22, 0x9b],
                 callbackA   = sinon.spy(),
                 callbackB   = sinon.spy(),
-                connection = new Connection(driver);
+                connection;
+
+            driver.on.restore();
+            driver.emit.restore();
+
+            connection = new Connection(driver);
 
             address.getRaw.returns(rawAddress);
-            message.getDestination.returns(address);
+            msg.getDestination.returns(address);
 
             connection.on(address, callbackA);
             connection.on(address, callbackB);
 
-            connection._onDriverMessage(message);
-
-            assert.ok(callbackA.calledOnce, "Callback A called");
-            assert.equal(callbackA.firstCall.args[0], message, "Message passed");
-            assert.ok(callbackB.calledOnce, "Callback B called");
+            driver.emit("message", msg);
+            expect(callbackA).to.be.calledOnce.and.calledWith(msg);
+            expect(callbackB).to.be.calledOnce.and.calledWith(msg);
         });
     });
-    
 });
