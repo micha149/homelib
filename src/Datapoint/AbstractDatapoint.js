3619f67bb20e604abbcc0e76d32b8ebe86ce6ccf
Michael van Engelshoven <michael@van-engelshoven.de>
2014-01-21 08:14:37 +0100

    Fixed usage of arguments.calee and internal renaming of DatapointType

--- src/Datapoint/AbstractDatapoint.js
+++ src/Datapoint/AbstractDatapoint.js
@@ -1,22 +1,22 @@
-function AbstractDatapoint(options) {
-    if (this.constructor === arguments.callee) {
-        throw new Error('An AbstractDatapoint can not be instanciated directly');
+function AbstractType(options) {
+    if (this.constructor === AbstractType) {
+        throw new Error('An AbstractType can not be instantiated directly');
     }
 
     this._id = options.id;
     this._name = options.name;
 }
 
-AbstractDatapoint.prototype.getId = function() {
+AbstractType.prototype.getId = function() {
     return this._id;
 };
 
-AbstractDatapoint.prototype.getType = function() {
+AbstractType.prototype.getType = function() {
     return this._type;
 };
 
-AbstractDatapoint.prototype.getName = function() {
+AbstractType.prototype.getName = function() {
     return this._name;
 };
 
-module.exports = AbstractDatapoint;
+module.exports = AbstractType;
---
+++ test/Datapoint/AbstractTypeTest.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect,
+    util = require('util'),
+    homelib = require('../../homelib'),
+    AbstractType = homelib.Datapoint.AbstractDatapoint;
+
+describe('Datapoint.AbstractType', function() {
+
+    describe('creating an instance', function() {
+
+        it('throws error if abstract type is directly instantiated', function() {
+            expect(function() {
+                var type = new AbstractType();
+            }).to.throw(Error);
+        });
+
+        it('does not throw error on derivations', function() {
+            function Derivation() {
+                AbstractType.apply(this, arguments);
+            }
+            util.inherits(Derivation, AbstractType);
+
+            expect(function() {
+                var type = new Derivation({id: null, 'name': null});
+            }).not.to.throw(Error);
+        });
+
+    });
+});
